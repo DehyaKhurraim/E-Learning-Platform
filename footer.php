50b9f318f7889b34ab514a354c52c40d14c0a6ce
Dehya Khurraim Siddiqui <57868646+DehyaKhurraim@users.noreply.github.com>
2023-06-03 02:04:53 +0500

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+
+   &copy; copyright @ <?= date('Y'); ?> by <span>E Guru.</span> | all rights reserved!
+
+</footer>
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/style1.css">
+
+</head>
+
+<body>
+
+    <div class="intro-section" id="home-section">
+        <div class="slide-1" style="background-image: url('images/hero_1.jpg');" data-stellar-background-ratio="0.5">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-12">
+                        <div class="sign">
+                            <div class="container" id=" container">
+                                <div class="form-container sign-in-container">
+                                    <form action="admin/login.php" method="post" id="signin_form">
+                                        <h1 class="title" style="font-size: 2rem;">Hello, Tutor!</h1>
+                                        <br>
+                                        <h1 class="title" style="font-size: medium; font-weight: 400;">Enter your personal details and start journey with us</h1>
+                                        <br>
+                                        <button style="cursor: pointer;">Sign Up</button>
+                                    </form>
+                                </div>
+                                <form action="login.php" method="post">
+                                    <div class="overlay-container">
+                                        <div class="overlay">
+                                            <div class="overlay-panel overlay-right">
+
+                                                <h1 style="font-size: 2rem;">Hello, Learner!</h1>
+                                                <br>
+                                                <h1 style="font-size: medium; font-weight: 400;">Enter your personal details and start journey with us</p>
+                                                    <button class="ghost" id="signUp" style="cursor: pointer;">Sign Up</button>
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
